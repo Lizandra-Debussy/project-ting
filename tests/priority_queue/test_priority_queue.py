b0c0fbd62486dabafedc7db049de9e99b0d72f7a
Lizandra Debussy <lizandradebussy@hotmail.com>
2023-04-06 09:31:56 -0300

    feat: Implementa a função exists_word

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,32 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+# import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+
+    file = [
+        {"qtd_linhas": 2},
+        {"qtd_linhas": 3},
+        {"qtd_linhas": 8},
+    ]
+
+    for f in file:
+        pq.enqueue(f)
+
+    assert len(pq) == len(file)
+    assert pq.search(0) == file[0]
+    assert pq.search(1) == file[1]
+    assert pq.search(2) == file[2]
+    pq.dequeue()
+    assert pq.search(0) == file[2]
+
+    # with pytest.assertRaises(IndexError):
+    #     pq.search(4)
+
+    # assert pq.search(1) == file[2]
+
+    # if pq.high_priority or pq.regular_priority:
+    #     assert len(pq) == len(file)
+    # else:
+    #     assert False, "A fila está vazia!"
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+    for file_index in range(len(instance)):
+        file = instance.search(file_index)
+        file_occurrences = [
+            {"linha": line_index + 1}
+            for line_index, line in enumerate(file["linhas_do_arquivo"])
+            if word.lower() in line.lower()
+        ]
+        if file_occurrences:
+            results.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": file_occurrences
+            })
+    return results
 
 
 def search_by_word(word, instance):
